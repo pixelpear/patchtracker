2bb311a5659c44e1dc0baa6be30a5d3314347b6f
Alena Bekrina <abekrina@gmail.com>
2018-06-13 12:19:20 +0200

    Made cell layout and moved to API 21

--- app/src/main/java/me/bekrina/patchtracker/GridAdapter.java
+++ app/src/main/java/me/bekrina/patchtracker/GridAdapter.java
@@ -2,12 +2,14 @@ package me.bekrina.patchtracker;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -22,11 +24,13 @@ public class GridAdapter extends ArrayAdapter {
     private List<Date> visibleDates;
     private Calendar currentCalendar;
     private List<Event> events;
+    private Context context;
     public GridAdapter(Context context, List<Date> visibleDates, Calendar currentCalendar, List<Event> events) {
         super(context, R.layout.single_cell_layout);
         this.visibleDates = visibleDates;
         this.currentCalendar = currentCalendar;
         this.events = events;
+        this.context = context;
         mInflater = LayoutInflater.from(context);
     }
     @NonNull
@@ -53,21 +57,18 @@ public class GridAdapter extends ArrayAdapter {
         }
 
         // Add day to calendar
-        TextView cellNumber = (TextView)view.findViewById(R.id.calendar_date_id);
-        cellNumber.setText(String.valueOf(dayOfMonth));
+        TextView cell = (TextView)view.findViewById(R.id.calendar_date_id);
+        cell.setText(String.valueOf(dayOfMonth));
 
         //Add events to the calendar
-        TextView eventIndicator = (TextView)view.findViewById(R.id.event_id);
         Calendar eventCalendar = Calendar.getInstance();
         for (int i = 0; i < events.size(); i++) {
             eventCalendar.setTime(events.get(i).getDate());
             if (dayOfMonth == eventCalendar.get(Calendar.DAY_OF_MONTH) && dateMonth == eventCalendar.get(Calendar.MONTH) + 1
                     && dateYear == eventCalendar.get(Calendar.YEAR)) {
-                eventIndicator.setBackgroundColor(Color.parseColor("#FFFFFF"));
-            }
-            if (dayOfMonth == eventCalendar.get(Calendar.DAY_OF_MONTH) && dateMonth == eventCalendar.get(Calendar.MONTH) + 1
-                    && dateYear == eventCalendar.get(Calendar.YEAR)) {
-                eventIndicator.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                // Note: set viewport proportions equal (height/width)
+                Drawable myDrawable = context.getDrawable(R.drawable.patch_normal);
+                cell.setBackground(myDrawable);
             }
         }
         return view;
--- app/src/main/java/me/bekrina/patchtracker/model/Event.java
+++ app/src/main/java/me/bekrina/patchtracker/model/Event.java
@@ -5,4 +5,6 @@ import java.util.Date;
 public interface Event {
     Date getDate();
     void setDate(Date date);
+    PatchEvent.EventType getType();
+    void setType(PatchEvent.EventType type);
 }
