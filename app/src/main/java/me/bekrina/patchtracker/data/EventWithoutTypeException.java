7a74a30533c6ca128a968f1da7e3fff96559f76d
Alena Bekrina <abekrina@gmail.com>
2018-07-24 13:50:05 +0200

    Layout with guidelines and barriers

--- app/src/main/java/me/bekrina/patchtracker/data/EventWithoutTypeException.java
+++
@@ -1,7 +0,0 @@
-package me.bekrina.patchtracker.data;
-
-public class EventWithoutTypeException extends Exception {
-    public EventWithoutTypeException (String message) {
-        super(message);
-    }
-}
