dcec2ed706ddd93516bce6025747849c3945b3cd
Alena Bekrina <abekrina@gmail.com>
2018-08-07 16:47:57 +0200

    EventInfoDialog layout in progress

--- app/src/main/java/me/bekrina/patchtracker/EventInfoDialog.java
+++ app/src/main/java/me/bekrina/patchtracker/EventInfoDialog.java
@@ -7,9 +7,14 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.threeten.bp.OffsetDateTime;
 
 import me.bekrina.patchtracker.data.Event;
 
@@ -28,20 +33,33 @@ public class EventInfoDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        event = (Event) getArguments().getSerializable(EVENT_KEY);
+        event = (Event) getArguments().getParcelable(EVENT_KEY);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.dialog_eventinfo, null));
+        View dialogView = inflater.inflate(R.layout.dialog_eventinfo, null);
+        TextView message = dialogView.findViewById(R.id.message);
+        message.setText(event.getType().toString());
+        OffsetDateTime eventDateTime = event.getDate();
+        TextView time = dialogView.findViewById(R.id.time);
+        String timeString = String.valueOf(eventDateTime.getHour()) + ":"
+                + String.valueOf(eventDateTime.getMinute());
+        time.setText(timeString);
+        TextView date = dialogView.findViewById(R.id.date);
+        String dateString = String.valueOf(eventDateTime.getDayOfMonth()) + "."
+                + String.valueOf(eventDateTime.getMonthValue()) + "."
+                + String.valueOf(eventDateTime.getYear());
+        date.setText(dateString);
+        builder.setView(dialogView);
         // Add action buttons
             builder.setPositiveButton(R.string.editevent, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent intent = new Intent("me.bekrina.patchtracker.EditEventActivity");
-                intent.putExtra("event", event);
-                startActivity(intent);
-            }
-        })
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent("me.bekrina.patchtracker.EditEventActivity");
+                    intent.putExtra("event", event);
+                    startActivity(intent);
+                }
+            })
                 .setNegativeButton(R.string.delete, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         EventInfoDialog.this.getDialog().cancel();
